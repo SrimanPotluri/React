36acece252280dc55429530a4c1f0c853a768dfa
spotluriZw <46686815+spotluriZw@users.noreply.github.com>
2020-01-09 11:14:53 -0500

    ES6 class components to functional stateless components

--- src/App.js
+++ src/App.js
@@ -62,53 +62,46 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <div>
+//Coverting Search, Table, Button ES6 class components to functional stateless components
+//These functional stateless components have no lifecycle methods except for render method that will be applied implicitly.
+
+function Search({ value, onChange, children }) {
+  return (
+    <div>
+      {children}
+      <input type="text" value={value} onChange={onChange} />
+    </div>
+  );
+}
+
+function Table({ list, pattern, onDismiss }) {
+  return (
+    <div>
+      {list.filter(isSearched(pattern)).map(item => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}> {item.title}</a>{" "}
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Button({ onClick, children }) {
+  return (
+    <div>
+      <button type="button" onClick={onClick}>
         {children}
-        <input type="text" value={value} onChange={onChange} />
-      </div>
-    );
-  }
-}
-
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}> {item.title}</a>{" "}
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
-            </span>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
-
-class Button extends Component {
-  render() {
-    const { onClick, children } = this.props;
-
-    return (
-      <div>
-        <button type="button" onClick={onClick}>
-          {children}
-        </button>
-      </div>
-    );
-  }
+      </button>
+    </div>
+  );
 }
 
 export default App;
