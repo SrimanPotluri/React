d2b4690781a3d2b37ba33da57cfdf53f6c422c15
spotluriZw <46686815+spotluriZw@users.noreply.github.com>
2020-01-13 12:21:43 -0500

    Real API data

--- src/App.js
+++ src/App.js
@@ -2,37 +2,36 @@ import React, { Component } from "react";
 
 import "./App.css";
 
+const DEFAULT_QUERY = "redux";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+
 //higher order function (a function that returns another function)
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: "",
-      list
+      searchTerm: DEFAULT_QUERY,
+      result: null
     };
   }
+
+  componentDidMount = () => {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  };
+
+  setSearchTopStories = result => {
+    this.setState({ result });
+  };
 
   onChangeSearch = event => {
     this.setState({ searchTerm: event.target.value });
@@ -49,7 +48,10 @@ class App extends Component {
   };
 
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    //render null if the list is empty
+    if (!result) return null;
 
     return (
       <div className="page">
@@ -58,7 +60,11 @@ class App extends Component {
             Search{" "}
           </Search>
         </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        <Table
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
