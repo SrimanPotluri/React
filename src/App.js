16f8f45c8a324f41a8a760c6d5a8d8a5e6d757b2
spotluriZw <46686815+spotluriZw@users.noreply.github.com>
2020-01-14 14:50:20 -0500

    paginated fetch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const DEFAULT_QUERY = "redux";
 const PATH_BASE = "https://hn.algolia.com/api/v1";
 const PATH_SEARCH = "/search";
 const PARAM_SEARCH = "query=";
+const PARAM_PAGE = "page=";
+const DEFAULT_HPP = "100";
+const PARAM_HPP = "hitsPerPage=";
 
 class App extends Component {
   constructor(props) {
@@ -21,15 +24,22 @@ class App extends Component {
     this.fetchSearchTopStories(searchTerm);
   };
 
-  fetchSearchTopStories = searchTerm => {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories = (searchTerm, page = 0) => {
+    fetch(
+      `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
+    )
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
   };
 
   setSearchTopStories = result => {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [...oldHits, ...hits];
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
   };
 
   onSearchSubmit = event => {
@@ -56,6 +66,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
 
     //render null if the list is empty
 
@@ -71,6 +82,11 @@ class App extends Component {
           </Search>
         </div>
         {result && <Table list={result.hits} onDismiss={this.onDismiss} />}
+        <Button
+          onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}
+        >
+          More
+        </Button>
       </div>
     );
   }
